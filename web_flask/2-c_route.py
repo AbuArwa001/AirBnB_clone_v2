42510f2d2956bb256882d31e780cbfa804d1b3c4
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-20 03:44:16 +0300

    ROute hbnb

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
     """returns TEXT WITH C concatnated"""
-    return f'C {text}'
+    return f'C {text.replace('_', ' ')}'
 
 
 if __name__ == '__main__':
