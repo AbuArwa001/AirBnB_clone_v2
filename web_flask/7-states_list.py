be1a5e2ce363a1daecb0742e0f6b10fc8b6d55b2
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-21 11:54:54 +0300

    Revert "Return to priveouc 18hr commit"
    This reverts commit ca752559ed645b06136d99e01d7d0feb8c6cd1a8.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+starts a Flask web application
 """
+
 from flask import Flask, render_template
 from models import storage
-
-
+import operator
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all('State')
+    """display a HTML page with the states listed in alphabetical order"""
+    
+    states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
