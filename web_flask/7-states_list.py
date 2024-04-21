ca285e1163fcdf704f784e94b3348f85aa0b6eb7
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-21 13:28:55 +0300

    Refactor html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+starts a Flask web application
 """
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from operator import itemgetter
-
-
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-
-    states_dict = storage.all('State')
-    states = sorted(states_dict.values(), key=itemgetter('name'))
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy Session after each request."""
+    """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
